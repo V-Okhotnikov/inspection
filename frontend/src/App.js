59a89f347792cd54405fb4486c9b0225bb96c19d
V-Okhotnikov <okhotnikov.va@gmail.com>
2025-10-10 09:03:22 +1100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import { AssetIntegritySection, StaticEquipmentSection, DigitalTransformSection } from './components/ServiceSection';
+import TurnaroundSection from './components/TurnaroundSection';
+import LessonsLearnedSection from './components/LessonsLearnedSection';
+import AboutSection from './components/AboutSection';
+import Footer from './components/Footer';
+import { Toaster } from './components/ui/toaster';
 
 function App() {
   return (
@@ -55,3 +62,4 @@ function App() {
 }
 
 export default App;
+
