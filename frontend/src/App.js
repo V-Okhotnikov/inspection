4e378c8717aa4f451f8fa7c6dd52949db038b4c2
V-Okhotnikov <okhotnikov.va@gmail.com>
2025-10-10 07:32:53 +1100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,18 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import { AssetIntegeritySection, StaticEquipmentSection, DigitalTransformSection } from './components/ServiceSection';
-import TurnaroundSection from './components/TurnaroundSection';
-import LessonsLearnedSection from './components/LessonsLearnedSection';
-import AboutSection from './components/AboutSection';
-import Footer from './components/Footer';
-//import { Toaster } from './components/ui/Toaster';
-
-const Home = () => {
-    return (
-        <div className="min-h-screen bg-white">
-            <Header />
-            <HeroSection />
-            <AssetIntegritySection />
-            <StaticEquipmentSection />
-            <TurnaroundSection />
-            <DigitalTransformSection />
-            <LessonsLearnedSection />
-            <AboutSection />
-            <Footer />
-        </div>
-    );
-}
+import React from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
-            //<Toaster />
-        </div>
-    );
+  return (
+    <div style={{
+      padding: '50px',
+      textAlign: 'center',
+      backgroundColor: '#4CAF50',
+      color: 'white',
+      minHeight: '100vh',
+      fontSize: '2rem'
+    }}>
+      ✅ React is Working!
+    </div>
+  );
 }
 
 export default App;
-
-
-
