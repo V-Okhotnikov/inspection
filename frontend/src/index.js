377995058b8664d5ef11ad52553916a3504e87a6
V-Okhotnikov <okhotnikov.va@gmail.com>
2025-10-10 08:47:20 +1100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,11 @@
-import React from "react";
-import "./App.css";
-import Header from './components/Header';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-function App() {
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center">
-          Inspection RBI Platform
-        </h1>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
