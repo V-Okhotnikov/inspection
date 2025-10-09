d93fe4159962bc5b66bfe4f6f465f05b23476c05
V-Okhotnikov <okhotnikov.va@gmail.com>
2025-10-09 17:33:11 +1100

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,40 +3,40 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
-import { AssetIntegritySection, StaticEquipmentSection, DigitalTransformSection } from './components/ServiceSection';
+import { AssetIntegeritySection, StaticEquipmentSection, DigitalTransformSection } from './components/ServiceSection';
 import TurnaroundSection from './components/TurnaroundSection';
 import LessonsLearnedSection from './components/LessonsLearnedSection';
 import AboutSection from './components/AboutSection';
 import Footer from './components/Footer';
-import { Toaster } from './components/ui/toaster';
+import { Toaster } from './components/ui/Toaster';
 
 const Home = () => {
-  return (
-    <div className="min-h-screen bg-white">
-      <Header />
-      <HeroSection />
-      <AssetIntegritySection />
-      <StaticEquipmentSection />
-      <TurnaroundSection />
-      <DigitalTransformSection />
-      <LessonsLearnedSection />
-      <AboutSection />
-      <Footer />
-    </div>
-  );
-};
+    return (
+        <div className="min-h-screen bg-white">
+            <Header />
+            <HeroSection />
+            <AssetIntegeritySection />
+            <StaticEquipmentSection />
+            <TurnaroundSection />
+            <DigitalTransformSection />
+            <LessonsLearnedSection />
+            <AboutSection />
+            <Footer />
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+            <Toaster />
+        </div>
+    );
 }
 
 export default App;
