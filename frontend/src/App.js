33e9141831be04fb60b2b80eb7f811f6090c02f1
V-Okhotnikov <okhotnikov.va@gmail.com>
2025-12-06 09:45:32 +1100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
@@ -49,6 +50,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
